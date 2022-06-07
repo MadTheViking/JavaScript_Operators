611648b3487bd983316883eefc5ce77800e600eb
David Truesdale <David8872Truesdale@hotmail.com>
2022-06-07 10:14:40 -0500

    assignment

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+12 * 365.25;
+let b = 4
+let z = a + b
+let num =  z
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
-
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
